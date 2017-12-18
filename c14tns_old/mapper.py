f3ad9b81bebbe4eeeaaa8761be2790895c36bdad
Tobias Nebaeus <c14tns@cs.umu.se>
2017-12-18 17:05:26 +0100

    Update non-working mapper

--- c14tns_old/mapper.py
+++ c14tns_old/mapper.py
@@ -1,6 +1,9 @@
 
 # TODO check if the position of the laser is equal to the robot's position
 
+import time
+from util import *
+from show_map import ShowMap
 
 class Mapper:
     """
@@ -29,21 +32,25 @@ class Map:
         self._scale = scale # "squares per meter"
         self._real_width = real_width
         self._real_height = real_height
-        map_width = real_width * scale
-        map_height = real_height * scale
+        map_width = int(real_width * scale)
+        map_height = int(real_height * scale)
+        print(map_width)
+        print(map_height)
         self._map =  [[0 for x in range(map_width)] for y in range(map_height)]
 
-    def get_occupancy(x, y):
+    def get_occupancy(self, x, y):
         # TODO check bounds, raise exception
-        grid_x = x / scale
-        grid_y = y / scale
+        grid_x = int(x / self._scale)
+        grid_y = int(y / self._scale)
         return _map[grid_x][grid_y]
 
-    def set_occupancy(x, y, value):
+    def set_occupancy(self, x, y, value):
         # TODO check bounds, raise exception
-        grid_x = x / scale
-        grid_y = y / scale
-        _map[grid_x][grid_y] = value;
+        print(x)
+        print(y)
+        grid_x = int(x / self._scale)
+        grid_y = int(y / self._scale)
+        self._map[grid_x][grid_y] = value;
 
 
 class LaserModel:
@@ -55,18 +62,20 @@ class LaserModel:
     def __init__(self, laser_angles):
         self._laser_angles = laser_angles
 
-    def apply_model(map, pose, laser_scan):
+    def apply_model(self, grid, pose, laser_scan):
         # use the laser scan to update the map using
         # the sensor model
 
-
         ### TEMP ###
-        robot_x = ...
-        robot_y = ...
-        length = laser_scan[135]
+        robot_x = pose['Pose']['Position']['X']
+        robot_y = pose['Pose']['Position']['Y']
+        length = laser_scan['Echoes'][135] # straight forward
         angle = 0
-        laser_hit_x = robot_x + length * sin(angle)
-        laser_hit_y = robot_y + length * cos(angle)
+        laser_hit_x = robot_x + length * cos(angle)
+        laser_hit_y = robot_y + length * sin(angle)
+        print("robot_x: " + str(robot_x) + " robot_y: " + str(robot_y))
+        print("laser_x: " + str(laser_hit_x) + " laser_y: " + str(laser_hit_y))
+        grid.set_occupancy(laser_hit_x, laser_hit_y, 15)
         ############
 
         # set occupancy around where the laser hit using some
@@ -74,3 +83,16 @@ class LaserModel:
         # set occupancy in a straight line to the laser hit, using
         # the line-drawing algorithm suggested in the specification
         pass
+
+# Temporary testing code
+if __name__ == '__main__':
+    occupancy_map = Map(10, 10, 0.05)
+    showmap_map = ShowMap(500, 500, True) # rows, cols, showgui
+    laser_angles = getLaserAngles()
+    laser_model = LaserModel(laser_angles)
+    while True:
+        laser_scan = getLaser()
+        pose = getPose()
+        laser_model.apply_model(occupancy_map, pose, laser_scan)
+        showmap_map.updateMap(occupancy_map._map, 15, 0, 0)
+        time.sleep(5)
---
+++ c14tns_old/show_map.py
@@ -0,0 +1,116 @@
+from  PIL import Image
+import numpy as np
+import time
+import threading
+
+"""
+ShowMap creates a Gui for showing the progress of the created map and saves it to file every 5 second
+Author Peter Hohnloser
+"""
+
+class ShowMap(object):
+    def __init__(self, gridHeight, gridWidth, showGUI):
+        """
+        Constructor for ShowMap
+
+        Args:
+            param gridHeight the height of the grid (no. of rows)
+            param gridWidth the width of the grid (no. of columns)
+            param ShowGUI if true showing the map
+        """
+        import matplotlib
+        if not showGUI:
+            matplotlib.use('Agg')
+        import matplotlib.pyplot as plt
+
+        self.saveMapTime = 5.0
+        self.mapName = 'map.png'
+        self.first = True
+        self.__robot_size = 6
+        self.__size = (gridHeight, gridWidth)
+
+        # create a grayscale image
+        data = np.ones(shape=self.__size)
+        self.__image = Image.fromarray(data * 0.5 * 255)
+
+        # remove the toolbar from plot
+        plt.rcParams['toolbar'] = 'None'
+
+        # using matplotlib to show an image in a subplot
+        self.__fig, self.__ax = plt.subplots(1, 1)
+        self.__fig.suptitle('Show Map')
+
+        # remove the x and y tick in figure
+        self.__ax.set_xticks([])
+        self.__ax.set_yticks([])
+
+        # Show image window
+        self.__implot = self.__ax.imshow(self.__image)
+
+        plt.show(block=False)
+        self.__fig.canvas.draw()
+
+        saveMap(self.__fig, self.mapName)
+        self.start_time = time.time()
+
+    def updateMap(self, grid, maxValue, robot_row, robot_col):
+        """
+        Creates a new BufferedImage from a grid with integer values between 0 - maxVal,
+        where 0 is black and maxVal is white, with a grey scale in between. Negative values are shown as gray.
+        Call this Method after you have updated the grid.
+
+        Args:
+            param grid is the updated grid (numpy matrix or a two-dimensional array)
+            param maxVal is the max value that is used in the grid
+            param robot_row is the current position of the robot in grid row
+            param robot_col is the current position of the robot in grid column
+        """
+        # convert grid to a numpy matrix
+        grid = np.matrix(grid)
+        # mapping the grid to an Image
+        for col in range(0, self.__size[1]):
+            for row in range(0, self.__size[0]):
+                value = grid[row, col]
+                # if value is <0 draw a gray pixel else mapping the value between 0 - 255
+                # where 0 is black and 255 is white
+                if value < 0:
+                    # set pixel value to gray
+                    self.__image.putpixel((col, row), 127)
+                else:
+                    # set pixel value
+                    self.__image.putpixel((col, row), abs(value * 255 / maxValue - 255))
+
+        # update the plot withe new image
+        self.__ax.clear()
+        self.__implot = self.__ax.imshow(self.__image)
+
+        # remove the x and y tick in figure
+        self.__ax.set_xticks([])
+        self.__ax.set_yticks([])
+
+        # plot the robot pose
+        self.__ax.plot((robot_col), (robot_row), 'rs', markersize=self.__robot_size)
+
+        # draw new figure
+        self.__fig.canvas.draw()
+
+        # Start a time that saves the image ever n seconds
+        elapsed_time = time.time() - self.start_time
+        if elapsed_time >= self.saveMapTime:
+            self.t = threading.Thread(target=saveMap, args=(self.__fig, self.mapName,))
+            self.t.start()
+            self.start_time = time.time()
+
+    def close(self):
+        """ Saves the last image before closing the application """
+        import matplotlib.pyplot as plt
+        saveMap(self.__fig, self.mapName)
+        plt.close()
+
+def saveMap(fig, mapName):
+    """ Saves the drawn Map to an Image """
+    data = np.fromstring(fig.canvas.tostring_rgb(), dtype=np.uint8, sep='')
+    data = data.reshape(fig.canvas.get_width_height()[::-1] + (3,))
+    img = Image.fromarray(data)
+    img.convert('RGB').save(mapName, 'PNG')
+
