434ee74546a0b7b0f4ae3878f0de5d143b0f500e
CyrilBos <bos.cyril33@gmail.com>
2017-12-22 18:05:45 +0100

    small optimization

--- src/mapper/laser_model.py
+++ src/mapper/laser_model.py
@@ -66,24 +66,27 @@ class LaserModel:
             logger.debug("robot_pos.x: " + str(robot_pos.x) + " robot_pos.y: " + str(robot_pos.y))
             logger.debug("laser_x: " + str(laser_hit_x) + " laser_y: " + str(laser_hit_y))
 
-            robot_cell = grid.convert_to_grid_indexes(robot_pos.x, robot_pos.y)
-            logger.info(robot_cell)
-            # region 1 = hit cell
             hit_cell = grid.convert_to_grid_indexes(laser_hit_x, laser_hit_y)
-            # alpha angle = 0
-            # occupied_probability = (((R - r) / R) + 1) / 2 * self._p_max
-            # grid.set_occupancy(laser_hit_x, laser_hit_y, occupied_probability)
-
-            # logger.info("probability hit cell {}".format(occupied_probability))
-
             logger.info("hit cell [{}][{}]".format(hit_cell[0], hit_cell[1]))
-            ############ seems that beta = 0.5 degrees is not enough of a difference to get a different cell
-            # TODO: might want to try with a higher resolution map
 
-            # region 2 = cells between the robot cell and the hit cell
-            self.bresenham_line(hit_cell, robot_cell, grid, R)
-                # set occupancy in a straight line to the laser hit, using
-                # the line-drawing algorithm suggested in the specification
+            if grid.is_in_bounds(hit_cell): #TODO: should be removed if we can manage negative indexes
+                robot_cell = grid.convert_to_grid_indexes(robot_pos.x, robot_pos.y)
+                logger.info("hit cell [{}][{}]".format(robot_cell[0], robot_cell[1]))
+                # region 1 = hit cell
+                # alpha angle = 0
+                occupied_probability = (((R - r) / R) + 1) / 2 * self._p_max
+                grid.set_occupancy(laser_hit_x, laser_hit_y, self.bayesian_probability(occupied_probability,
+                                                                                       grid.get_occupancy(laser_hit_x, laser_hit_y)))
+
+                # logger.info("probability hit cell {}".format(occupied_probability))
+
+                ############ seems that beta = 0.5 degrees is not enough of a difference to get a different cell
+                # TODO: might want to try with a higher resolution map
+
+                # region 2 = cells between the robot cell and the hit cell
+                self.bresenham_line(hit_cell, robot_cell, grid, R)
+                    # set occupancy in a straight line to the laser hit, using
+                    # the line-drawing algorithm suggested in the specification
 
     def bresenham_line(self, hit_cell, robot_cell, grid, R):
         deltax = hit_cell[0] - robot_cell[0]
