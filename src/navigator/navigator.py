8db578f0a30c600fa8e256aec22c104729964f72
arban <arbano.gmail.com>
2017-12-25 23:30:47 +0100

    Add other required modules

---
+++ src/navigator/navigator.py
@@ -0,0 +1,14 @@
+
+
+class Navigator:
+    """
+    The path planner, resposible for creating a path to the
+    goal set by the planner.
+
+    Wayfront or A*?
+
+    Input: a goal position / grid coordinate?
+
+    Output: a path to the goal, avoiding obstacles?
+    """
+    pass
---
+++ src/planner/planner.py
@@ -0,0 +1,15 @@
+
+
+class Planner:
+    """
+    Responsible for choosing the next area to explore.
+
+    Maybe use a second grid to keep track of what areas have been explored?
+
+    Frontier-based exploration?
+
+    Input: a CSpace grid produced by the mapper?
+
+    Output: A goal position (or grid coordinate) to explore?
+    """
+    pass
