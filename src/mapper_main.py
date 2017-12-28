8f1eb93a7c88a12796cd627ad864a580ae1ea89f
CyrilBos <bos.cyril33@gmail.com>
2017-12-28 16:24:23 +0100

    started threading the different modules

--- src/mapper_main.py
+++ src/mapper_main.py
@@ -1,7 +1,7 @@
 # Temporary testing code
 
 import logging
-
+from threading import Thread
 
 logging.basicConfig(format="[%(asctime)s %(filename)s:%(lineno)s - %(funcName)20s() ] %(message)s",
                     level=logging.INFO)
@@ -12,11 +12,33 @@ from mapper import Map
 from mapper import ShowMap
 
 
-from controller import Controller
-
+from controller import Controller, FixedController
 
 from planner.planner import Planner
 import time
+
+# TODO: remove once path planning works
+from controller.path_loader import PathLoader
+
+path_filepath = 'paths/Path-around-table-and-back.json'
+path = PathLoader().loadPath(path_filepath)
+
+def mapping_routine():
+    while True:
+        laser_scan = controller.get_laser_scan()
+        pos, rot = controller.get_pos_and_orientation()
+        laser_model.apply_model(occupancy_map, pos, rot, laser_scan)
+        robot_indexes = occupancy_map.convert_to_grid_indexes(pos.x, pos.y)
+        planner = Planner(occupancy_map)
+        goal_point = (0, 0)
+        p = planner.closest_frontier_centroid(robot_indexes)
+
+        if p is not None:
+            goal_point = p
+
+        # TODO: p max getter
+        showmap_map.updateMap(occupancy_map.grid(), laser_model._p_max, robot_indexes[0], robot_indexes[1], goal_point)
+        time.sleep(0.01)
 
 
 if __name__ == '__main__':
@@ -43,22 +65,10 @@ if __name__ == '__main__':
     occupancy_map = Map(x1, y1, x2, y2, scale)
     showmap_map = ShowMap(scale * width, scale * height, True)  # rows, cols, showgui
 
-    controller = Controller(mrds_url)
+    controller = FixedController(mrds_url)
     laser_angles = controller.get_laser_scan_angles()
     laser_model = LaserModel(laser_angles, max_distance)
 
-    while True:
-        laser_scan = controller.get_laser_scan()
-        pos, rot = controller.get_pos_and_orientation()
-        laser_model.apply_model(occupancy_map, pos, rot, laser_scan)
-        robot_indexes = occupancy_map.convert_to_grid_indexes(pos.x, pos.y)
-        planner = Planner(occupancy_map)
-        goal_point = (0, 0)
-        p = planner.closest_frontier_centroid(robot_indexes)
-
-        if p is not None:
-            goal_point = p
-
-        # TODO: p max getter
-        showmap_map.updateMap(occupancy_map.grid(), laser_model._p_max, robot_indexes[0], robot_indexes[1], goal_point)
-        time.sleep(0.01)
+    mapping_thread = Thread(target=mapping_routine())
+    mapping_thread.start()
+    controlling_thread = Thread(target=controller.pure_pursuit(path))
